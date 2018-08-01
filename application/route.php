518fca21418dcd080731e85437804d2f8476d209
吴云飞 <771687865@qq.com>
2018-08-01 13:22:43 +0800

    小程序

---
+++ application/route.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+/*
+return [
+    '__pattern__' => [
+        'name' => '\w+',
+    ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
+    ],
+
+];*/
+use think\Route;
+
+//Route::rule('路由表达式','路由地址','请求类型','路由参数（数组）','变量规则（数组）')
+
+//GET PUT POST DELETE
+
+Route::rule('index','index/index/index','GET');
